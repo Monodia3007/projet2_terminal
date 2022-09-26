ebe7458a7cfd81a99363330f10d5c462a3fc5777
Redrock18 <114251326+Redrock18@users.noreply.github.com>
2022-09-26 10:29:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Page: connexion.php
+*/
+//à mettre tout en haut du fichier .php, cette fonction propre à PHP servira à maintenir la $_SESSION
+session_start();
+//si le bouton "Connexion" est cliqué
+if(isset($_POST['connexion'])){
+    // on vérifie que le champ "Pseudo" n'est pas vide
+    // empty vérifie à la fois si le champ est vide et si le champ existe belle et bien (is set)
+    if(empty($_POST['pseudo'])){
+        echo "Le champ Pseudo est vide.";
+    } else {
+        // on vérifie maintenant si le champ "Mot de passe" n'est pas vide"
+        if(empty($_POST['mdp'])){
+            echo "Le champ Mot de passe est vide.";
+        } else {
+            // les champs pseudo & mdp sont bien postés et pas vides, on sécurise les données entrées par l'utilisateur
+            //le htmlentities() passera les guillemets en entités HTML, ce qui empêchera en partie, les injections SQL
+            $Pseudo = htmlentities($_POST['pseudo'], ENT_QUOTES, "UTF-8"); 
+            $MotDePasse = htmlentities($_POST['mdp'], ENT_QUOTES, "UTF-8");
+            //on se connecte à la base de données:
+            $mysqli = mysqli_connect("domaine.tld", "nom d'utilisateur", "mot de passe", "base de données");
+            //on vérifie que la connexion s'effectue correctement:
+            if(!$mysqli){
+                echo "Erreur de connexion à la base de données.";
+            } else {
+                //on fait maintenant la requête dans la base de données pour rechercher si ces données existent et correspondent:
+                //si vous avez enregistré le mot de passe en md5() il vous faudra faire la vérification en mettant mdp = '".md5($MotDePasse)."' au lieu de mdp = '".$MotDePasse."'
+                $Requete = mysqli_query($mysqli,"SELECT * FROM membres WHERE pseudo = '".$Pseudo."' AND mdp = '".$MotDePasse."'");
+                //si il y a un résultat, mysqli_num_rows() nous donnera alors 1
+                //si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
+                if(mysqli_num_rows($Requete) == 0) {
+                    echo "Le pseudo ou le mot de passe est incorrect, le compte n'a pas été trouvé.";
+                } else {
+                    //on ouvre la session avec $_SESSION:
+                    //la session peut être appelée différemment et son contenu aussi peut être autre chose que le pseudo
+                    $_SESSION['pseudo'] = $Pseudo;
+                    echo "Vous êtes à présent connecté !";
+                }
+            }
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Reeeeee</title>
+</head>
+<body>
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="#">Thème</a>
+        <a href="#">Auteur</a>
+        <a href="connexion.php">Connexion</a>
+        <a href="inscription.php">Inscription</a>
+    </div>
+      
+    <!-- Use any element to open the sidenav -->
+    <input type="button" value="Open" name="sidebar button" onclick="openNav()"/>
+      
+    <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+    <div id="main">
+        <h1>Bienvenue sur mon site... Connard!</h1>
+        <p>eeeeeeeeeeeee</p>
+    </div> 
+    
+</body>
+<script src="scripts.js"></script>
+</html>
---
+++ inscription.php
@@ -0,0 +1,70 @@
+<?php  
+    /* page: inscription.php */
+//connexion à la base de données:
+$BDD = array();
+$BDD['host'] = "localhost";
+$BDD['user'] = "root";
+$BDD['pass'] = "pierre";
+$BDD['db'] = "nom_de_la_base_de_donnees";
+$mysqli = mysqli_connect($BDD['host'], $BDD['user'], $BDD['pass'], $BDD['db']);
+if(!$mysqli) {
+    echo "Connexion non établie.";
+    exit;
+}
+    //création automatique de la table membres, une fois créée, vous pouvez supprimer les lignes de code suivantes:
+    echo mysqli_query($mysqli,"CREATE TABLE IF NOT EXISTS `".$BDD['db']."`.`membres` ( `id` INT NOT NULL AUTO_INCREMENT , `pseudo` VARCHAR(25) NOT NULL , `mdp` CHAR(32) NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;")?"Table membres créée avec succès, vous pouvez maintenant supprimer la ligne ". __LINE__ ." de votre fichier ". __FILE__ ."!":"Erreur création table membres: ".mysqli_error($mysqli);
+    //la table est créée avec les paramètres suivants:
+    //champ "id": en auto increment pour un id unique, peux vous servir pour une identification future
+    //champ "pseudo": en varchar de 0 à 25 caractères
+    //champ "mdp": en char fixe de 32 caractères, soit la longueur de la fonction md5()
+    //fin création automatique
+//par défaut, on affiche le formulaire (quand il validera le formulaire sans erreur avec l'inscription validée, on l'affichera plus)
+$AfficherFormulaire=1;
+//traitement du formulaire:
+if(isset($_POST['pseudo'],$_POST['mdp'])){//l'utilisateur à cliqué sur "S'inscrire", on demande donc si les champs sont défini avec "isset"
+    if(empty($_POST['pseudo'])){//le champ pseudo est vide, on arrête l'exécution du script et on affiche un message d'erreur
+        echo "Le champ Pseudo est vide.";
+    } elseif(!preg_match("#^[a-z0-9]+$#",$_POST['pseudo'])){//le champ pseudo est renseigné mais ne convient pas au format qu'on souhaite qu'il soit, soit: que des lettres minuscule + des chiffres (je préfère personnellement enregistrer le pseudo de mes membres en minuscule afin de ne pas avoir deux pseudo identique mais différents comme par exemple: Admin et admin)
+        echo "Le Pseudo doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";
+    } elseif(strlen($_POST['pseudo'])>25){//le pseudo est trop long, il dépasse 25 caractères
+        echo "Le pseudo est trop long, il dépasse 25 caractères.";
+    } elseif(empty($_POST['mdp'])){//le champ mot de passe est vide
+        echo "Le champ Mot de passe est vide.";
+    } elseif(mysqli_num_rows(mysqli_query($mysqli,"SELECT * FROM membres WHERE pseudo='".$_POST['pseudo']."'"))==1){//on vérifie que ce pseudo n'est pas déjà utilisé par un autre membre
+        echo "Ce pseudo est déjà utilisé.";
+    } else {
+        //toutes les vérifications sont faites, on passe à l'enregistrement dans la base de données:
+        //Bien évidement il s'agit là d'un script simplifié au maximum, libre à vous de rajouter des conditions avant l'enregistrement comme la longueur minimum du mot de passe par exemple
+        if(!mysqli_query($mysqli,"INSERT INTO membres SET pseudo='".$_POST['pseudo']."', mdp='".md5($_POST['mdp'])."'")){//on crypte le mot de passe avec la fonction propre à PHP: md5()
+            echo "Une erreur s'est produite: ".mysqli_error($mysqli);//je conseille de ne pas afficher les erreurs aux visiteurs mais de l'enregistrer dans un fichier log
+        } else {
+            echo "Vous êtes inscrit avec succès!";
+            //on affiche plus le formulaire
+            $AfficherFormulaire=0;
+        }
+    }
+}
+if($AfficherFormulaire==1){
+    ?>
+    <!-- 
+    Les balises <form> sert à dire que c'est un formulaire
+    on lui demande de faire fonctionner la page inscription.php une fois le bouton "S'inscrire" cliqué
+    on lui dit également que c'est un formulaire de type "POST"
+     
+    Les balises <input> sont les champs de formulaire
+    type="text" sera du texte
+    type="password" sera des petits points noir (texte caché)
+    type="submit" sera un bouton pour valider le formulaire
+    name="nom de l'input" sert à le reconnaitre une fois le bouton submit cliqué, pour le code PHP
+     -->
+    <br />
+    <form method="post" action="inscription.php">
+        Pseudo (a-z0-9) : <input type="text" name="pseudo">
+        <br />
+        Mot de passe : <input type="password" name="mdp">
+        <br />
+        <input type="submit" value="S'inscrire">
+    </form>
+    <?php
+}
+?>
